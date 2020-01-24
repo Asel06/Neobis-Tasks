be6d805aabcb993a39256a8299325e8a032d01b2
Asel06 <Asel_06.2001@mail.ru>
2020-01-24 22:17:25 +0600

    Update task

--- task-2/JavaIfElse.java
+++ task-2/JavaIfElse.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class JavaIf-Else {
+public class JavaIfElse {
 
     private static final Scanner scanner = new Scanner(System.in);
 
